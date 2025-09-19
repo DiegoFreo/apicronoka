81ab1a6a9891fb92c15081327d071a78725f2087
DiegoFreo <diego_freo8@hotmail.com>
2025-09-18 22:39:44 -0300

    Arquivo Vercel

--- db/conect.js
+++ db/conect.js
@@ -4,11 +4,12 @@ require('dotenv').config();
   async function main()
   {
     try {
-      await mongoose.connect(process.env.MONGO_URL);
+      await mongoose.connect(process.env.MONGODB_URI);
       return mongoose;
     } catch (err) {
       console.error("Erro ao conectar ao MongoDB", err);
       process.exit(1);
     }
   }
+   
   module.exports = main();
