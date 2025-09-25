2e9caf3e24bd7dd0647a966f0a588535e62c78eb
DiegoFreo <diego_freo8@hotmail.com>
2025-09-25 19:11:47 -0300

    serverless 1

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+/*
 require('./models/piloto');
 require('./models/usuario');
 require('./models/categoria');
@@ -34,7 +34,40 @@ app.use('/api', routes);
 /*
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
-});*/
+});
 
+
+module.exports = serverless(app);
+*/
+
+require('./models/piloto');
+require('./models/usuario');
+require('./models/categoria');
+require('./models/evento');
+require('./models/bateria');
+require('./models/volta');
+require('dotenv').config();
+
+const Usuario = require('./models/usuario');
+
+const express = require('express');
+const cors = require('cors');
+const serverless = require('serverless-http');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const conectarDB = require('./db/conect');
+conectarDB.then(() => {
+    console.log('Banco de dados conectado');
+}).catch(err => {
+    console.error('Erro ao conectar ao banco de dados', err);
+    process.exit(1);
+});
+
+// Importa as rotas
+const routes = require('./routes/routes');
+app.use('/api', routes);
 
 module.exports = serverless(app);
