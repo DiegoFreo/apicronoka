4e08cbda9ccefa5ead0811b3a6b058276054ac92
DiegoFreo <diego_freo8@hotmail.com>
2025-09-25 21:04:43 -0300

    Mongopse

--- db/conect.js
+++ db/conect.js
@@ -1,25 +1,12 @@
 
 const mongoose = require('mongoose');
 require('dotenv').config();
-/*
-  async function main()
-  {
-    try {
-      await mongoose.connect(process.env.MONGODB_URI, );
-      return mongoose;
-    } catch (err) {
-      console.error("Erro ao conectar ao MongoDB", err);
-      process.exit(1);
-    }
-  }
-   
-  module.exports = main();
-  */
+
  let isConnected = false;
 
 async function main() {
   if (isConnected) return mongoose;
-  mongoose.set('bufferCommands', false);
+  //mongoose.set('bufferCommands', false);
 
   try {
     await mongoose.connect(process.env.MONGODB_URL);
