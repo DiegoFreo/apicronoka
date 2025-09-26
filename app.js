5f49a87391338566cbfcc50965acdeded77eddf7
DiegoFreo <diego_freo8@hotmail.com>
2025-09-25 21:23:29 -0300

    Mongopse

--- app.js
+++ app.js
@@ -49,28 +49,34 @@ require('./models/volta');
 */
 require('dotenv').config();
 
-//const Usuario = require('./models/usuario');
-
 const express = require('express');
 const cors = require('cors');
 const serverless = require('serverless-http');
+const conectarDB = require('./db/conect');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const conectarDB = require('./db/conect');
-conectarDB.then(() => {
-    console.log('Banco de dados conectado');
-}).catch(err => {
-    console.error('Erro ao conectar ao banco de dados', err);
-    //process.exit(1);
-});
 
 // Importa as rotas
 const routes = require('./routes/routes');
 app.use('/api', routes);
+let dbConectado = false;
 
+app.use(async (req, res, next) => {
+  if (!dbConectado) {
+    try {
+      await conectarDB;
+      dbConectado = true;
+      console.log('Banco de dados conectado');
+    } catch (err) {
+      console.error('Erro ao conectar ao banco de dados', err);
+      return res.status(500).json({ error: 'Erro ao conectar ao banco de dados' });
+    }
+  }
+  next();
+});
 
 
 module.exports = serverless(app);
