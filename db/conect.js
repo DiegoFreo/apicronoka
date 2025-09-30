5665ee9a7c48c7745afa84a9907dc8fcc55ab82f
DiegoFreo <diego_freo8@hotmail.com>
2025-09-30 19:07:20 -0300

    Mudança do app.js

--- db/conect.js
+++ db/conect.js
@@ -1,5 +1,48 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+global.mongoose = mongoose || {conn: null, promise: null};
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+}
+
+async function conectarDB() {
+  const cacheado = global.mongoose;
+  if (cacheado.conn) {
+    return cacheado.conn;
+  }
+  if (cacheado.promise) {
+      const opts = {
+        maxPoolSize: 5,
+        minPoolsize: 1,
+        bufferCommands: false,
+        bufferMaxEntries: 0,
+        ServerSelectionTimeoutMS: 5000,
+      };
+      cacheado.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+        return mongoose;
+      }).catch((err) => {
+        global.mongoose.promise = null;
+        console.error('Erro ao conectar ao MongoDB:', err);
+        throw err;
+      });
+  }
+  try {
+    cacheado.conn = await cacheado.promise;
+  } catch (err) {
+    cacheado.promise = null;
+    throw err;
+  } 
+  return cacheado.conn;
+ 
+}
+module.exports = conectarDB;
+
+/*
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 let isConnected = false;
 
@@ -25,5 +68,5 @@ async function main() {
   }
 }
 
-module.exports = main();
+module.exports = main();*/
 
