90736214fbc34d6874a53aa226cc31043ceda0e3
DiegoFreo <diego_freo8@hotmail.com>
2025-10-05 20:58:56 -0300

    Mudança para import

--- db/conect.js
+++ db/conect.js
@@ -51,6 +51,7 @@ try {
 module.exports = conectarDB;
 */
 
+/*
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -79,5 +80,43 @@ const conectarDB = async()=> {
 }
 
 module.exports =  conectarDB;
+*/
 
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const conectarDB = async () => {
+  if (cached.conn) {
+    // Já existe conexão ativa
+    return cached.conn;
+  }
+
+  if (!process.env.MONGODB_URI) {
+    throw new Error('MONGODB_URI não está definida no .env');
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      connectTimeoutMS: 10000, // 10 segundos
+      serverSelectionTimeoutMS: 10000
+    };
+
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, opts).then((mongoose) => {
+      console.log('✅ MongoDB conectado');
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
+
+module.exports = conectarDB;
 
